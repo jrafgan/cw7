1a80f17ffd14741fbea090ef3cf8ec65a0eef58c
Maksat Duishenaliev <maksatovy@gmail.com>
2018-12-29 11:28:17 +0600

    work on task started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App mainDiv">
+        <div className="orderDetails">Order Details
+        </div>
+        <div className="foodList">Food List</div>
+      </div>
+    );
+  }
+}
+
+export default App;
